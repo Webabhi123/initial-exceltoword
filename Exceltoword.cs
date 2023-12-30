3207c308f090e7446efa63b3b9fb89394eca5761
Webabhi123 <146459033+Webabhi123@users.noreply.github.com>
2023-12-30 19:11:22 +0530

    Add files via upload

---
+++ Exceltoword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    class Exceltoword
+    {
+        public class ExcelRowData
+        {
+            public string FirstName { get; set; }
+            public string  LastName { get; set; }
+            public string PhoneNumber { get; set; }
+
+            public string Village { get; set; }
+
+            // Add more properties for other columns as needed
+            // Example: public string Column2 { get; set; }
+        }
+    }
+}
